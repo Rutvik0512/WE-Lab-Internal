cac5de57aacb22560fbce4f96f655f352aae1cbd
Rutvik0512 <88983725+Rutvik0512@users.noreply.github.com>
2023-05-30 12:28:11 +0530

    Delete Events Module directory

--- Events Module/event.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-const events = require("events")
-const myEmitter = new events.EventEmitter()
-
-myEmitter.on("TEST_EVENT", msg => {
-    console.log(msg)
-})
-
-myEmitter.emit("TEST_EVENT","Event Triggered")
